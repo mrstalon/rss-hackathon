3dc5b7df3b334a9f25e6344fd02339c0aa366fc1
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2019-01-26 21:17:23 +0300

    rss-hackaton: poet information add, new info structure

--- client/content/poets-en.js
+++ client/content/poets-en.js
@@ -1,31 +1,14 @@
 export default [
   {
-    /*id: 100,
-    name: '',
-    avatarName: 'poets_.jpg',
-    biographyContent: [
-      {
-        born: '',
-        period: '',
-        genre: '',
-        description: "",
-        composition: [
-          { id: 1, name: '', year: '19' },
-          { id: 2, name: '', year: '19' },
-          { id: 3, name: '', year: '19' },
-          { id: 4, name: '', year: '19' },
-          { id: 5, name: '', year: '19' }
-        ]
-      }
-    ]
-  },*/
-  {
     id: 0,
     name: 'Pyotr Ustinovich Brovka',
     avatarName: 'poets_PyotrUstinovichBrovka.jpg',
     biographyContent: [
       {
-        born: 'Putilkovichi, Russian empire',
+        born: {
+          city: 'Putilkovichi',
+          country: 'Russian empire'
+        },
         period: '1905 - 1980',
         genre: 'lyric poetry, novel',
         description: "Brovka was born into a large (but poor) family in Putilkovichi, a settlement in present-day Ushachy Raion, Vitebsk Region, on June 25, 1905. His first published works were printed in 1926, and the young poet briefly served as executive secretary of a local newspaper in 1927. In 1928 he enrolled in the Literature-Linguistics Department of Belarusian State University, graduating in 1931. Brovka joined the Communist Party of the Soviet Union in 1940. \nDuring the Second World War, Brovka's mother was among the Belarusian people sent to Auschwitz by the occupying Nazi forces. Brovka himself wrote for the partisan press and published in the army newspapers, lauding the fighting effort of the Soviet people. He was recognized by the Soviet government with the Stalin Prize in 1947, the same year that Brovka was elected chairman of the Writers' Union of the Byelorussian SSR (a position that he would hold until 1967). \nBrovka reputation as a Soviet and Belarusian poet continued to grow during Brovka's subsequent decades in Minsk after the war. The fullest apex of his literary talents is considered to be his final decades in the 1960s and 1970s. \nBrovka was awarded the prestigious Lenin Prize after his completion of the poetry volume And Time Goes On in 1962. In 1966, Brovka edited the first volume of the Byelorussian Soviet Encyclopedia, the year he was given the title of People's Poet of the Byelorussian Soviet Socialist Republic. He was elected a full member of the Belarusian Academy of Sciences in 1966. \nApart from literary work, he served several terms as a representative of the Supreme Soviet of the Soviet Union, but maintained his literary career even as a political delegate. Some of his later verses made their way into Soviet pop music when they were adapted by the Belarusian Soviet folk rock band Pesniary.",
@@ -35,7 +18,8 @@ export default [
           { id: 3, name: 'You are my bee', year: '1972' },
           { id: 4, name: 'When rivers merge', year: '1957' },
           { id: 5, name: 'Young friends', year: '1961' }
-        ]
+        ],
+        video: 'https://www.youtube.com/watch?v=y7q6WXFqP2I'
       }
     ]
   },
@@ -45,7 +29,10 @@ export default [
     avatarName: 'poets_AnatolVolny.jpg',
     biographyContent: [
       {
-        born: 'Pukhavichy, Russian empire',
+        born: {
+          city: 'Pukhavichy',
+          country: 'Russian empire'
+        },
         period: '1902-1937',
         genre: 'drama',
         description: "Anatol' Volny (Belarusian, Анатоль Вольны, 1902 - 29 October 1937) was a Belarusian artist, poet, writer and journalist. During the Great Purge, he became a victim of the 1937 mass execution of Belarusians. Posthumously rehabilitated in 1957.",
@@ -53,7 +40,8 @@ export default [
           { id: 1, name: 'Collection of feuilletons', year: '1927' },
           { id: 2, name: 'To you: poems', year: '1927' },
           { id: 3, name: 'Neighbors: Gumoreski and feuilletons', year: '1932' }
-        ]
+        ],
+        video: 'https://www.youtube.com/watch?v=V5qnr3_cTf8'
       }
     ]
   },
@@ -63,7 +51,10 @@ export default [
     avatarName: 'poets_AlesHarun.jpg',
     biographyContent: [
       {
-        born: 'Minsk, Russian empire',
+        born: {
+          city: 'Minsk',
+          country: 'Russian empire'
+        },
         period: '1887-1920',
         genre: 'prose, drama',
         description: "He was born on February 27 (March 11) 1887 in Minsk. His father, Uladzimir Prušynski and mother Sophia (née. Zhivitsa) were simple workers' representatives of the Catholic community in Minsk. Since childhood, He loved books and five years could read in Russian and Polish. Later he graduated from the Minsk third city parish (1897) and craft (1902. He worked in the carpentry and furniture factory in Minsk. From 1904 to join the party, the Socialist-Revolutionaries maximalists. He began to publish in 1907 in the newspaper "Nasha Niva". He was arrested for anti-government activities on March 4, 1907 in the underground press on Broad Street in Minsk, which was printed at the time an appeal "to all workers." In July 1908 the Vilnius Court of Justice condemned the link and lifelong exile in Siberia. Exiled to Irkutsk Province (Rus.) Russian. From 1914 he received the right to travel throughout Siberia. Working on a barge on the Lena River, prepared a compilation of "Parent gift" and in 1914 sent him to Vilnius (released in 1918). After working in the gold mines in Badayba. Ales Harun imbued with the idea of social and national emancipation of the Belarusian people.",
@@ -72,7 +63,56 @@ export default [
           { id: 2, name: 'Mother Belarus', year: '1907' },
           { id: 3, name: 'Night thoughts', year: '1908' },
           { id: 4, name: 'Mother’s gift and other writings', year: '1962' }
-        ]
+        ],
+        video: 'https://www.youtube.com/watch?v=6bOw-U-ezuw'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Adam Hlobus',
+      avatarName: 'poets_AdamHlobus.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Dzyarzhynsk',
+          country: 'Byelorussian SSR'
+        },
+        period: '1958-...',
+        genre: 'story, essay',
+        description: "He was born in Dzyarzhynsk Minsk Region in the family of Belarusian writer Vyacheslav Adamchyk (Belarusian: Vyachaslau Adamchyk). From 1959 and living in Minsk e. He graduated from the pedagogical department Minsk Art School them. A. K. Glebov (1977), art department Belarusian Theatre and Art Institute (1983). He worked as a draftsman, painter and restorer, a graphic designer, editor of \"Krynitsa\" (from 1987). It is engaged in publishing activities ( \"Suchasnyj lіtaratar\" publishing house). \nWife - Alena Adamchyk, famous Belarusian photographer. He has two children and a grandson.\nHe was one of the founding members of the informal association of young writers \"Tuteishyja\" (Tuteishyja, 1986 - 1990), which is due to the circumstances involved, not only in literature, but also an expression of and an active social and political position. In particular, in October 1988 and the union organized, which later became the traditional, primarily in Belarus share commemoration Soviet regime called \"Forefathers Eve\", with many thousands march and rally. Member of the Writer\'s Union (1988) Union of Belarusian Writers (with the 1989 a).",
+        composition: [
+          { id: 1, name: 'Park', year: '1988' },
+          { id: 2, name: 'Death - a man', year: '1992' },
+          { id: 3, name: 'Crossroads', year: '1993' },
+          { id: 4, name: 'Notebooks', year: '2003' },
+          { id: 5, name: 'Contemporaries', year: '2006' }
+        ],
+        video: 'https://www.youtube.com/watch?v=-L3wz8QQbAs'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Nil Hilevich',
+    avatarName: 'poets_NilHilevich.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Slabada',
+          country: 'Belarusian SSR'
+        },
+        period: '1931-2016',
+        genre: 'poetry',
+        description: "Nil Hilevich was born in the village of Slabada , in the Lahoisk District of Mensk Province. He studied in a college in Mensk (from which he graduated in 1951), preparing to be a teacher. During the last year in college he worked as a teacher in one of the schools in Mensk. He continued his education at the Belarusian State University (the Faculty of Philology), and graduated in 1956. During 1960-1986 he was working at the university, and later on became a professor. In 1958 he started to work for a newspaper Zvyazda. In 1978 Hilevich joined the Communist Party. In 1980 he became the executive secretary of the Writer's Union of BSSR, and held that position for 9 years. In 1989 he became a chairperson of the Frantsishak Skaryna Belarusian Language Society. He also was the chief editor of the Society's bulletin Our Word. In 1991 Nil Hilevich received a People's Poet of Belarus nomination. He has also received some other awards in literature.",
+        composition: [
+          { id: 1, name: '', year: '19' },
+          { id: 2, name: '', year: '19' },
+          { id: 3, name: '', year: '19' },
+          { id: 4, name: '', year: '19' },
+          { id: 5, name: '', year: '19' }
+        ],
+        video: ''
       }
     ]
   },
