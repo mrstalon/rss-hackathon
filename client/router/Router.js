d25c1d2978d30d559296781c1d71cb28bde09e61
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2019-01-26 15:30:36 +0300

    feat: implement header with nav-bar

--- client/router/Router.js
+++ client/router/Router.js
@@ -5,14 +5,19 @@ import MainPage from '../views/MainPage/MainPage'
 import SearchPage from '../views/SearchPage/SearchPage'
 import PoetPage from '../views/PoetPage/PoetPage'
 
+import NavBar from '../views/MainPage/components/Header/components/NavBar'
+
 function Router() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-        <Route path="/search" component={SearchPage} />
-        <Route path="/poet" component={PoetPage} />
-      </Switch>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" component={MainPage} exact />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/poet" component={PoetPage} />
+        </Switch>
+      </div>
     </BrowserRouter>
   )
 }
