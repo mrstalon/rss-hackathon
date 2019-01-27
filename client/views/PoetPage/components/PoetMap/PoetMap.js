662fa4ac0dda54889fda67b8a893a05681ed932a
V1ctoR <v1ctor.belikov@gmail.com>
2019-01-27 20:19:42 +0300

    fix: poets-by.js, poets-en.js

--- client/views/PoetPage/components/PoetMap/PoetMap.js
+++ client/views/PoetPage/components/PoetMap/PoetMap.js
@@ -1,33 +1,39 @@
-import React from 'react'
-
+import React, { Component } from 'react'
 import './poet-map.scss'
+import GoogleMapReact from 'google-map-react'
 
 class PoetMap extends React.Component {
   state = {
-    content: {
-      ru: {
-        title: 'Привет из карты поэта'
-      },
-      en: {
-        title: 'Hello from poet map'
-      }
-      // 'by': {
-      //   title: '...'
-      // }
-    },
-
     // по дефолту язык будет русским
     currentContent: null
+  }
+
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
   }
 
   render() {
     const { content } = this.state
     const { lang } = this.props
-    const currentContent = content[lang]
 
     return (
-      <div className="poet__map--container">
+      <div className="poet__map--container" style={{ height: '100vh', width: '100%' }}>
         <h2 className="poet__map--title">Места деятельности</h2>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
       </div>
     )
   }
