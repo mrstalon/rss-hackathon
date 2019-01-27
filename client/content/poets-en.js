52c32576949154d2e0245535efa646f476a8bc6c
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2019-01-27 10:44:41 +0300

    fix: fix content-em

---
+++ client/content/poets-en.js
@@ -0,0 +1,143 @@
+export default [
+  {
+    id: 0,
+    name: 'Pyotr Ustinovich Brovka',
+    avatarName: 'poets_PyotrUstinovichBrovka.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Putilkovichi',
+          country: 'Russian empire'
+        },
+        period: '1905 - 1980',
+        genre: 'lyric poetry, novel',
+        description: "Brovka was born into a large (but poor) family in Putilkovichi, a settlement in present-day Ushachy Raion, Vitebsk Region, on June 25, 1905. His first published works were printed in 1926, and the young poet briefly served as executive secretary of a local newspaper in 1927. In 1928 he enrolled in the Literature-Linguistics Department of Belarusian State University, graduating in 1931. Brovka joined the Communist Party of the Soviet Union in 1940. \nDuring the Second World War, Brovka's mother was among the Belarusian people sent to Auschwitz by the occupying Nazi forces. Brovka himself wrote for the partisan press and published in the army newspapers, lauding the fighting effort of the Soviet people. He was recognized by the Soviet government with the Stalin Prize in 1947, the same year that Brovka was elected chairman of the Writers' Union of the Byelorussian SSR (a position that he would hold until 1967). \nBrovka reputation as a Soviet and Belarusian poet continued to grow during Brovka's subsequent decades in Minsk after the war. The fullest apex of his literary talents is considered to be his final decades in the 1960s and 1970s. \nBrovka was awarded the prestigious Lenin Prize after his completion of the poetry volume And Time Goes On in 1962. In 1966, Brovka edited the first volume of the Byelorussian Soviet Encyclopedia, the year he was given the title of People's Poet of the Byelorussian Soviet Socialist Republic. He was elected a full member of the Belarusian Academy of Sciences in 1966. \nApart from literary work, he served several terms as a representative of the Supreme Soviet of the Soviet Union, but maintained his literary career even as a political delegate. Some of his later verses made their way into Soviet pop music when they were adapted by the Belarusian Soviet folk rock band Pesniary.",
+        composition: [
+          { id: 1, name: 'Years like a storm', year: '1930' },
+          { id: 2, name: 'And the days go…', year: '1961' },
+          { id: 3, name: 'You are my bee', year: '1972' },
+          { id: 4, name: 'When rivers merge', year: '1957' },
+          { id: 5, name: 'Young friends', year: '1961' }
+        ],
+        video: 'https://www.youtube.com/watch?v=y7q6WXFqP2I'
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Anatol Volny',
+    avatarName: 'poets_AnatolVolny.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Pukhavichy',
+          country: 'Russian empire'
+        },
+        period: '1902-1937',
+        genre: 'drama',
+        description: "Anatol' Volny (Belarusian, Анатоль Вольны, 1902 - 29 October 1937) was a Belarusian artist, poet, writer and journalist. During the Great Purge, he became a victim of the 1937 mass execution of Belarusians. Posthumously rehabilitated in 1957.",
+        composition: [
+          { id: 1, name: 'Collection of feuilletons', year: '1927' },
+          { id: 2, name: 'To you: poems', year: '1927' },
+          { id: 3, name: 'Neighbors: Gumoreski and feuilletons', year: '1932' }
+        ],
+        video: 'https://www.youtube.com/watch?v=V5qnr3_cTf8'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ales Harun',
+    avatarName: 'poets_AlesHarun.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Minsk',
+          country: 'Russian empire'
+        },
+        period: '1887-1920',
+        genre: 'prose, drama',
+        description: "He was born on February 27 (March 11) 1887 in Minsk. His father, Uladzimir Prušynski and mother Sophia (née. Zhivitsa) were simple workers' representatives of the Catholic community in Minsk. Since childhood, He loved books and five years could read in Russian and Polish. Later he graduated from the Minsk third city parish (1897) and craft (1902. He worked in the carpentry and furniture factory in Minsk. From 1904 to join the party, the Socialist-Revolutionaries maximalists. He began to publish in 1907 in the newspaper "Nasha Niva". He was arrested for anti-government activities on March 4, 1907 in the underground press on Broad Street in Minsk, which was printed at the time an appeal "to all workers." In July 1908 the Vilnius Court of Justice condemned the link and lifelong exile in Siberia. Exiled to Irkutsk Province (Rus.) Russian. From 1914 he received the right to travel throughout Siberia. Working on a barge on the Lena River, prepared a compilation of "Parent gift" and in 1914 sent him to Vilnius (released in 1918). After working in the gold mines in Badayba. Ales Harun imbued with the idea of social and national emancipation of the Belarusian people.",
+        composition: [
+          { id: 1, name: 'My carols', year: '1920' },
+          { id: 2, name: 'Mother Belarus', year: '1907' },
+          { id: 3, name: 'Night thoughts', year: '1908' },
+          { id: 4, name: 'Mother’s gift and other writings', year: '1962' }
+        ],
+        video: 'https://www.youtube.com/watch?v=6bOw-U-ezuw'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Adam Hlobus',
+      avatarName: 'poets_AdamHlobus.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Dzyarzhynsk',
+          country: 'Byelorussian SSR'
+        },
+        period: '1958-...',
+        genre: 'story, essay',
+        description: "He was born in Dzyarzhynsk Minsk Region in the family of Belarusian writer Vyacheslav Adamchyk (Belarusian: Vyachaslau Adamchyk). From 1959 and living in Minsk e. He graduated from the pedagogical department Minsk Art School them. A. K. Glebov (1977), art department Belarusian Theatre and Art Institute (1983). He worked as a draftsman, painter and restorer, a graphic designer, editor of \"Krynitsa\" (from 1987). It is engaged in publishing activities ( \"Suchasnyj lіtaratar\" publishing house). \nWife - Alena Adamchyk, famous Belarusian photographer. He has two children and a grandson.\nHe was one of the founding members of the informal association of young writers \"Tuteishyja\" (Tuteishyja, 1986 - 1990), which is due to the circumstances involved, not only in literature, but also an expression of and an active social and political position. In particular, in October 1988 and the union organized, which later became the traditional, primarily in Belarus share commemoration Soviet regime called \"Forefathers Eve\", with many thousands march and rally. Member of the Writer\'s Union (1988) Union of Belarusian Writers (with the 1989 a).",
+        composition: [
+          { id: 1, name: 'Park', year: '1988' },
+          { id: 2, name: 'Death - a man', year: '1992' },
+          { id: 3, name: 'Crossroads', year: '1993' },
+          { id: 4, name: 'Notebooks', year: '2003' },
+          { id: 5, name: 'Contemporaries', year: '2006' }
+        ],
+        video: 'https://www.youtube.com/watch?v=-L3wz8QQbAs'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Nil Hilevich',
+    avatarName: 'poets_NilHilevich.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Slabada',
+          country: 'Belarusian SSR'
+        },
+        period: '1931-2016',
+        genre: 'poetry',
+        description: "Nil Hilevich was born in the village of Slabada , in the Lahoisk District of Mensk Province. He studied in a college in Mensk (from which he graduated in 1951), preparing to be a teacher. During the last year in college he worked as a teacher in one of the schools in Mensk. He continued his education at the Belarusian State University (the Faculty of Philology), and graduated in 1956. During 1960-1986 he was working at the university, and later on became a professor. In 1958 he started to work for a newspaper Zvyazda. In 1978 Hilevich joined the Communist Party. In 1980 he became the executive secretary of the Writer's Union of BSSR, and held that position for 9 years. In 1989 he became a chairperson of the Frantsishak Skaryna Belarusian Language Society. He also was the chief editor of the Society's bulletin Our Word. In 1991 Nil Hilevich received a People's Poet of Belarus nomination. He has also received some other awards in literature.",
+        composition: [
+          { id: 1, name: 'The Jack of Diamonds', year: '1961' },
+          { id: 2, name: 'To New Wreaths', year: '1963' },
+          { id: 3, name: 'A Sin or Two', year: '1970' },
+          { id: 4, name: 'How I Learnt to Live', year: '1974' },
+          { id: 5, name: 'Mermaid in the Narach', year: '1974' }
+        ],
+        video: 'https://www.youtube.com/watch?v=sWPk3sk6m6k'
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Larysa Hienijus',
+    avatarName: 'poets_LarysaHienijus.jpg',
+    biographyContent: [
+      {
+        born: {
+          city: 'Zlobaucy',
+          country: 'Russian empire'
+        },
+        period: '1910-1983',
+        genre: 'poetry',
+        description: "She was born Larysa Miklasevic in the estate Zlobaucy (now Vaukavysk raion, Grodno Region) into the family of a wealthy land-owner. She had many siblings. Hienijus went to a Polish school, in 1928 she successfully completed Vaukavysk Polish Gymnasium. At that time, she got acquainted with the world literature - the Polish, Scandinavian and English classics. She also started writing poetry.\nOn 3 February she married a medical student, Janka Hienijus, who at that time was studying at Charles University, Prague. In 1937, after the birth of their son Jurka, she joined her husband in Prague. There, one of Hienijuss' neighbors was Alexandra Kosach-Shimanovskaya, sister of Lesya Ukrainka - the author whose work has greatly influenced the Belarusian poet. Hienijus's first published poems appeared in \"Раніца\", a Berlin newspaper of Belarusian emigrants in 1939. In 1942, her first collection of poetry, «Ад родных ніў» was published. It was filled with nostalgia and thoughts about the fate of her homeland.\nWhen in 1939 the Red Army took over western Belarus, the poet's father, Anton Miklasevic, was shot dead and his mother and two sisters were deported to Kazakhstan. According to the will of the then President of the Belarusian People's Republic (BNR) Vasil Zacharka, Larysa Hienijus was appointed Secretary General of the Government of BNR in exile in March 1943. She preserved and organised the BNR archive, took care of Belarusian immigrants, political refugees and prisoners of war. She hid the most valuable part of the archive in the location out of the reach of the NKVD and the MGB. Later, the Soviet security service tortured the poet to get hold of the archive.\nAfter the Second World War, Larysa Hienijuš with her husband and son lived in Vimperk near Prague. 5 March 1948, they were arrested by the MGB. Both Larysa and Janka Hienijuš were kept in the prisons in Czechoslovakia and Lviv, Ukraine. In October 1948 they were transferred to a prison in Minsk. Here Larysa Hienijuš was interrogated and tortured by the BSSR Minister of State Security Lavrentii Tsanava.\nOn 7 February 1949, both Hienijušes were sentenced to 25 years of imprisonment. The poet served her term in the camps of the Komi ASSR. She showed an example of a phenomenal spiritual strength by bringing together Belarusians and prisoners of other nationalities, and continued writing poetry. Prisoners called her Mother and her poems - glucose, memorising them by heart and reading as a prayer. After eight years, Larysa Hienijuš was released in 1956.\nThe tragic fate befell other members of Miklaševič family: the poet's mother and two sisters died in Kazakhstan in 1945; her brother, Arkadź, soldier in the Anders' Army, died on 27 July 1944 in the Battle of Monte Cassino in Italy, and another brother, Raścislaǔ, soldier in the Polish Army, died on 26 (28?) April 1945 near Berlin.\nAfter her release, the poet settled in her husband's home in Zelva. Both Hienijušes refused to accept the Soviet citizenship and the rest of their lives lived with a note "Stateless" in their passports. On several occasions they requested rehabilitation, but every time received a reply: “Sentenced lawfully”. KGB followed the poet everywhere, read the letters sent to her from all around the world, installed spying devices in her house. Janka Hienijuš was allowed to work as a doctor in the local clinic, and her wife - not for long - as a cleaner. In 1979, Janka died and the poet was allocated a meager pension. The authorities denied the poet a quiet old age; only once she was allowed to visit her son Jurka, who lived in Białystok, Poland.\nFor nearly ten years, the poet's works were totally banned. Only in 1963, her post-improsonment poems were published in Belarusian magazines for the first time. Thanks to the then Chairman of the Supreme Soviet of the BSSR, Maksim Tank, Larysa Hienijuš's first in Belarus collection of works, «Невадам зь Нёмана» was published in 1967. It was edited by Uladzimir Karatkievič pro bono. It included most of the poems from «Ад родных ніў» without some excerpts removed by censors, as well as new poems of the Zelva period. Then, for years she was allowed to publish only children’s poetry.\nLarysa Hienijuš died in 1983. Thousands of people came to her burial.\nIn 1999, the Belarusian Helsinki Committee appealed to the General Prosecutor's Office to annul an unjust sentence against both Hienijušs. The Office forwarded the appeal to the Supreme Court which refused the annulment as the poet \"has no grounds for rehabilitation.\" However, the reasons for refusal were unknown as, according to the Court's letter, \"they may only be given to the repressed person\".",
+        composition: [
+          { id: 1, name: 'From native cornfields', year: '1942' },
+          { id: 2, name: 'Tales for Mikhail', year: '1972' },
+          { id: 3, name: 'Good morning', year: '1976' },
+          { id: 4, name: 'Mother & son', year: '1992' },
+          { id: 5, name: 'Choice', year: '1997' }
+        ],
+        video: 'https://www.youtube.com/watch?v=nw4eWICjiyw'
+      }
+    ]
+  }
+]
