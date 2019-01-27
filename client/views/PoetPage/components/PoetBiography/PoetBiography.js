ca04f9c6bd075ce678165b09c27a29b18eabb5c7
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2019-01-27 21:00:31 +0300

    feat: add multiple lang support for PoetPage component and add poet render by choosed poet

--- client/views/PoetPage/components/PoetBiography/PoetBiography.js
+++ client/views/PoetPage/components/PoetBiography/PoetBiography.js
@@ -4,8 +4,24 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css'
 
 class PoetBiography extends React.Component {
+  state = {
+    content: {
+      ru: {
+        title: 'Биография:'
+      },
+      en: {
+        title: 'Biography:'
+      },
+      by: {
+        title: 'Быяграф'
+      }
+    }
+  }
+
   render() {
-    const { lang, description } = this.props
+    const { description, lang } = this.props
+    const { content } = this.state
+    const currentContent = content[lang]
 
     const elements = description.map((el) => {
       return (
@@ -22,7 +38,7 @@ class PoetBiography extends React.Component {
 
     return (
       <div className="poet__biography--container">
-        <h2 className="poet__biography--title">Биография</h2>
+        <h2 className="poet__biography--title">{currentContent.title}</h2>
         <VerticalTimeline>
           {elements}
           <VerticalTimelineElement iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }} />
--- client/views/PoetPage/components/PoetMap/PoetMap.js
+++ client/views/PoetPage/components/PoetMap/PoetMap.js
@@ -4,7 +4,17 @@ import GoogleMapReact from 'google-map-react'
 
 class PoetMap extends React.Component {
   state = {
-    // по дефолту язык будет русским
+    content: {
+      en: {
+        title: 'Working place'
+      },
+      ru: {
+        title: 'Места деятельности'
+      },
+      by: {
+        title: 'Месца работы'
+      }
+    },
     currentContent: null
   }
 
@@ -19,20 +29,21 @@ class PoetMap extends React.Component {
   render() {
     const { content } = this.state
     const { lang } = this.props
+    const currentContent = content[lang]
 
     return (
       <div className="poet__map--container" style={{ height: '100vh', width: '100%' }}>
-        <h2 className="poet__map--title">Места деятельности</h2>
+        <h2 className="poet__map--title">{currentContent.title}</h2>
         <GoogleMapReact
           // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
+          {/* <AnyReactComponent
             lat={59.955413}
             lng={30.337844}
             text={'Kreyser Avrora'}
-          />
+          /> */}
         </GoogleMapReact>
       </div>
     )
